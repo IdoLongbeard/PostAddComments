21217a0d8f392d521f2b5ad9304ea7ca8f6bda2b
IdoLongbeard <gabriel.mourare@protonmail.com>
2021-08-18 22:43:49 -0300

    First commit

---
+++ PostComment/PostComment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace PostComment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string moment ="21/06/2018 13:05:44";
+            string title = "Travelling to New Zealand!";
+            string content = "I'm going to visit this wonderful country!!";
+            int likes = 12;
+
+            Convert.ToDateTime(moment);
+
+            Post post = new Post(moment, title, content, likes);
+
+            
+
+            Comment comment = new Comment("Have a nice trip!");
+
+            post.AddComment(comment);
+
+            Console.WriteLine(post);
+
+
+        }
+    }
+}
